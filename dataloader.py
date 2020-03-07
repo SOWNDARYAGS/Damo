14620e8c6fa12280f91bf96c3fe502e623d362d6
SOWNDARYAGS <sowndaryags@gmail.com>
2020-03-07 14:16:44 +0530

    fsdfd

---
+++ dataloader.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+import os
+
+
+#It generates cropped image from original captured image
+# directory='data2'
+# i=936
+# for filename in os.listdir(directory):
+#     if filename.endswith(".jpg") or filename.endswith(".png"):
+#         print(os.path.join(directory, filename))
+#         output=cv2.imread(os.path.join(directory, filename))
+        
+#         cropped=output[60:300,100:400]
+#         cv2.imshow("image",cropped)
+        
+#         cropped_img_rgb=cv2.cvtColor(cropped,cv2.COLOR_BGR2RGB)
+        
+       
+#         cv2.imwrite('cropped_data/output{}.jpg'.format(i),cropped)
+#         i=i+1
+
+#         cv2.waitKey(0)
+#     else:
+#         print("hye path does not exist")
+
